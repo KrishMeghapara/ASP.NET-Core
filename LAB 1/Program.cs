c15e54fff085ee25e597f94e1923f3d43a1d3665
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-13 20:25:40 +0530

    Add files via upload

---
+++ LAB 1/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lab 1 :  A - 1
+            //string name = Console.ReadLine();
+            //string address = Console.ReadLine();
+            //long ContactNumber = Convert.ToInt64(Console.ReadLine());
+            //string city = Console.ReadLine();
+
+
+
+            //Console.WriteLine(name);
+            //Console.WriteLine(address);
+            //Console.WriteLine(ContactNumber);
+            //Console.WriteLine(city);
+
+            // Lab 1 :   A - 2
+
+            //int Number1 = Convert.ToInt32(Console.ReadLine());
+            //int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(Number1);
+            //Console.WriteLine(Number2);
+
+
+            // Lab 1 :   A - 3
+
+            //string name = Console.ReadLine();
+            //string countryName = Console.ReadLine();
+
+            //Console.WriteLine("Hello"+" "+name+ " " +"From country" + " " + countryName);
+
+            // Lab 1 :   A - 4
+
+            //int length = Convert.ToInt32(Console.ReadLine());
+            //int width = Convert.ToInt32(Console.ReadLine());
+
+            //long area = length * width;
+            //Console.WriteLine(area);
+
+            // Lab 1 :   A - 5
+
+            // area for Square  : 
+
+            //int length = Convert.ToInt32(Console.ReadLine());
+
+            //long area = (length*length);
+
+            //Console.WriteLine(area);
+
+            // area for Rectangle 
+
+            //int length = Convert.ToInt32(Console.ReadLine());
+            //int width = Convert.ToInt32(Console.ReadLine());
+
+
+            //long area = length * width;
+            //Console.WriteLine(area);
+
+            // area for Circle
+
+            //int Redius = Convert.ToInt32(Console.ReadLine());
+
+            //Double Pie = 3.14; 
+
+            //double area = (Pie * Redius * Redius);
+
+            //Console.WriteLine(area);
+
+
+            // Lab 1 :   B - 6
+
+            //Console.WriteLine("Enter 1 for Converting Celsius To Fahrenhit OR Enter 2 for Convering Fahrenhit to Celsius .//")
+            //    int num = Convert.ToInt32(Console.ReadLine());
+
+            //void CToF() {
+            //    double Celsius = Convert.ToDouble(Console.ReadLine());
+
+
+            //    double Fahrenhit = (Celsius * 9 / 5) + 32;
+            //    Console.WriteLine(Fahrenhit);
+            //}
+            //void FToC()
+            //{
+            //    double Fahrenhit = Convert.ToDouble(Console.ReadLine());
+
+            //    double Celsius = (Fahrenhit - 32) * 5 / 9;
+            //    Console.WriteLine(Celsius);
+
+            //}
+
+            //if (num == 1) { 
+            //    CToF();
+            //}
+            //else
+            //{
+            //    FToC();
+            //}
+
+            // Lab 1 :   B - 7
+
+            //SimpleInterestCounter();
+
+            //void SimpleInterestCounter()
+            //{
+            //    //int P = Convert.ToInt32(Console.ReadLine());
+            //    //int N = Convert.ToInt32(Console.ReadLine());
+            //    //int R = Convert.ToInt32(Console.ReadLine());
+
+            //    //double SimpleInterest = P * N * R / 100;
+
+            //    //Console.WriteLine(SimpleInterest);
+            //}
+
+
+
+
+            // Lab 1 :   B - 8
+
+
+            //int Number1 = Convert.ToInt32(Console.ReadLine());
+            //int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            //String Key = Console.ReadLine();
+
+            // If-Else
+
+            //if (Key == "+")
+            //{
+            //    double ans = Number1 + Number2;
+            //    Console.WriteLine(ans);
+            //}
+            //else if (Key == "-")
+            //{
+            //    double ans = Number1 - Number2;
+            //    Console.WriteLine(ans);
+
+            //}
+            //else if(Key == "/"){
+            //    double ans = Number1 / Number2;
+            //    Console.WriteLine(ans);
+
+            //}
+            //else if(Key == "*"){
+            //    double ans = Number1 * Number2;
+            //    Console.WriteLine(ans);
+
+            // Switch
+
+            //switch (Key)
+            //{
+            //    case "+" :
+            //        double ans1 = Number1 + Number2;
+            //        Console.WriteLine(ans1);
+            //        break;
+            //    case "-" :
+            //        double ans2 = Number1 - Number2;
+            //        Console.WriteLine(ans2);
+            //        break;
+            //    case "/" :
+            //        double ans3 = Number1 / Number2;
+            //        Console.WriteLine(ans3);
+            //        break;
+            //    case "*" :
+            //        double ans4 = Number1 * Number2;
+            //        Console.WriteLine(ans4);
+            //        break;
+
+            //}
+
+            // Lab 1 :   C - 9
+
+            //int a = 10;
+            //int b = 20;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //a = a * b;
+            //b = a / b;
+            //a = a / b;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            // Lab 1 :   C - 10
+
+           // int a = 10;
+           // int b = 20;
+           // int c = 30;
+
+           //int max = (a > b ? (a > c ? a : b) : (b > c ? b : c));  
+        }
+    }
+}
