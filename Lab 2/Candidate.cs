21cde21c8a19177fb1a36e0f39393db444be6141
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-18 13:06:32 +0530

    Add files via upload

---
+++ Lab 2/Candidate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Candidate
+    {
+        int Id;
+        string Name;
+        int Age;
+        int Weight;
+        int Height;
+
+        public void GetCandidateDetails()
+        {
+            Id = Convert.ToInt32(Console.ReadLine());
+            Name = Console.ReadLine();
+            Age = Convert.ToInt32(Console.ReadLine());
+            Weight = Convert.ToInt32(Console.ReadLine());
+            Height = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayCandidateDetails()
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(Weight);
+            Console.WriteLine(Height);
+        }
+    }
+}
