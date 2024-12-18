4b28d500d8d0aed8ce39bd6faa5bd43b54d30a4c
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-18 13:06:45 +0530

    Add files via upload

---
+++ Lab 2/Staff.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Staff
+    {
+        String Name;
+        String Department;
+        String Designation;
+        int Experience;
+        int Salary;
+
+        public void GetStaffDetails()
+        {
+           
+            Name = Console.ReadLine();
+            Department = Console.ReadLine();
+            Designation = Console.ReadLine();
+            Experience = Convert.ToInt32(Console.ReadLine());
+            Salary = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayStaff()
+        {
+            if (Designation == "HOD") {
+                Console.WriteLine(Name);
+                Console.WriteLine(Salary);
+            }
+        }
+    }
+}
