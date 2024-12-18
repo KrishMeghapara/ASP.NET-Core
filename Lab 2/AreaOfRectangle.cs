194e1afeb6493c4a4dfdb8721c88924d97283a4b
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-18 13:05:13 +0530

    Add files via upload

---
+++ Lab 2/AreaOfRectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class AreaOfRectangle
+    {
+        int Weight;
+        int Height;
+        public AreaOfRectangle() {
+            Weight = Convert.ToInt32(Console.ReadLine());
+            Height = Convert.ToInt32(Console.ReadLine());
+
+            long Area = Weight * Height;
+            Console.WriteLine(Area);
+        }
+    }
+}
