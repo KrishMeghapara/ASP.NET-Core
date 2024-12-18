347882452393b077dfb572f245ecc94a51dfb913
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-18 13:06:26 +0530

    Add files via upload

---
+++ Lab 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Candidate c1 = new Candidate();
+            c1.GetCandidateDetails();
+            c1.DisplayCandidateDetails();
+
+            Staff[] staff = new Staff[5];
+            for (int i = 0; i < staff.Length; i++) {
+                staff[i]=new Staff();
+                staff[i].GetStaffDetails();
+
+            }
+            for (int i = 0; i < staff.Length; i++)
+            {
+                staff[i].DisplayStaff();
+
+            }
+
+            Bank_Account account = new Bank_Account();
+            account.GetBank_AccountDetails();
+            account.DisplayBank_AccountDetails();
+
+            Student student = new Student();
+            student.DisplayStudent();
+
+            AreaOfRectangle areaOfRectangle = new AreaOfRectangle();    
+
+        }
+    }
+}
