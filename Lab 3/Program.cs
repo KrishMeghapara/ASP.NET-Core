7a77e2ff12d5c12bae67c07a13b3431b3dd00d74
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-19 21:23:19 +0530

    Add files via upload

---
+++ Lab 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            zeroException z1 = new zeroException();
+            IndexOutOfRangeException z2 = new IndexOutOfRangeException();
+            Calculate c1 = new Calculate();
+            Result r1 = new Result();
+            StringClassMethods s2 = new StringClassMethods();
+        }
+    }
+}
