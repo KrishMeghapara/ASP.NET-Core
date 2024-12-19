8254e097ba8d56db8e0e6b66bbcce8c42cddf2ff
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-19 21:23:52 +0530

    Add files via upload

---
+++ Lab 3/StringClassMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class StringClassMethods
+    {
+        public StringClassMethods() {
+            String a = "Rudra Parsaniya";
+
+            Console.WriteLine(a.ToLower());
+            Console.WriteLine(a.ToUpper());
+            Console.WriteLine(a.Substring(2));
+            Console.WriteLine(a.Reverse());
+
+
+        }
+    }
+}
