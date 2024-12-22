9aef5dc44217fb07bb7dd7048d641e906caa6894
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2024-12-22 17:47:50 +0530

    Add files via upload

---
+++ Lab 2/Distance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Distance
+    {
+        int dist1;
+        int dist2;
+        int dist3;
+        public Distance() { 
+             dist1 = Convert.ToInt32(Console.ReadLine()); 
+             dist2 = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalculateDistance() 
+        {
+             dist3 = dist1 + dist2;
+        }
+        public void DisplayDistance(){
+            Console.WriteLine(dist1);
+            Console.WriteLine(dist2);
+            Console.WriteLine(dist3);
+        }
+    }
+}
